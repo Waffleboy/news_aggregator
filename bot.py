1b7322919c0dadb3598904e82eafb4666a7e3d45
Thiru <thiru1337@gmail.com>
2018-11-01 01:08:31 +0000

    fix empty news

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ def send_news_to_channel(bot,text,monthly = False):
     text_fluff = add_fluff_to_news_string(text,monthly)
     ## HACK: Fix too long message length
     text_chunks = message_length_fixer(text_fluff)
-    if len(text_chunks) == 0:
+    if len(text_chunks[0]) <= 2:
         bot.send_message(chat_id=CHANNEL_ID,text="No news in the last 24 hours.")
         return
     for chunk in text_chunks:
